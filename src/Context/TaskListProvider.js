3f4f323eed8224c71c9e6f28c71ec2f6950ba5e1
vasanth-selvaraj97 <vasanth.selvaraj@mavenberg.com>
2023-11-06 09:17:31 +0530

    Demo data removed

--- src/Context/TaskListProvider.js
+++ src/Context/TaskListProvider.js
@@ -3,23 +3,14 @@ import React, { createContext, useEffect, useState } from "react";
 const TaskListContext = createContext([]);
 
 const TaskListProvider = ({ children }) => {
-  const [taskList, setTaskList] = useState([{
-    id: "9999",
-    name: "Demo Task",
-    description: "Description for Demo Task",
-    due_date: "2023-11-09",
-    comment: "",
-    comments: ["1st Comment","Second Comment"],
-    state: "To Do",
-    completed_date: "",
-  }]);
+  const [taskList, setTaskList] = useState([]);
 
-  useEffect(()=>{
-    const tasks = JSON.parse(localStorage.getItem("taskList"))
-    if(tasks){
-        setTaskList(tasks)
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem("taskList"));
+    if (tasks) {
+      setTaskList(tasks);
     }
-  },[])
+  }, []);
 
   return (
     <TaskListContext.Provider value={{ taskList, setTaskList }}>
