3064ff4e9c1a8b58a9eeceb555a4812a6bdee0ca
vasanth-selvaraj97 <vasanth.selvaraj@mavenberg.com>
2023-11-11 15:16:08 +0530

    revert back

--- src/index.js
+++ src/index.js
@@ -4,18 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import HashRouter from "hash-router";
 import { ThemeProvider, TaskListProvider } from "./Context/ContextExport";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter basename={window.location.pathname || ""}>
+  <BrowserRouter basename={window.location.pathname || ""}>
     <ThemeProvider>
       <TaskListProvider>
         <App />
       </TaskListProvider>
     </ThemeProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 reportWebVitals();
