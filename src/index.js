c8e5b389e295e289c32e6e202556f5f8f8e9996d
vasanth-selvaraj97 <vasanth.selvaraj@mavenberg.com>
2023-11-11 15:43:36 +0530

    Added hash router for more routing possibilities

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider, TaskListProvider } from "./Context/ContextExport";
-
+// basename={window.location.pathname || ""}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={window.location.pathname || ""}>
+  <HashRouter >
     <ThemeProvider>
       <TaskListProvider>
         <App />
       </TaskListProvider>
     </ThemeProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 reportWebVitals();
